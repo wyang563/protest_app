f9746429f619ccacccb19ca66459da15f580189a
Alan Morelos <amorelos@college.harvard.edu>
2025-02-15 09:26:30 -0800

    diff port

--- backend/app.py
+++ backend/app.py
@@ -44,4 +44,4 @@ def transcribe_audio():
 
 if __name__ == '__main__':
     # Run on port 5000 so React (port 3000) can access it
-    app.run(host='0.0.0.0', port=5000, debug=True)
+    app.run(host='0.0.0.0', port=5001, debug=True)
